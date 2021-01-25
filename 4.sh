01b5e7fbe691ffdb42a1d4d3151fd70e643dc166
domorelivelonger <60047304+domorelivelonger@users.noreply.github.com>
2021-01-25 23:52:46 +0200

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -1,1 +1,18 @@
-dd
+cd /root/nginx-install/development
+git clone https://github.com/arut/nginx-rtmp-module.git
+git clone https://github.com/kaltura/nginx-vod-module.git
+
+wget https://ftp.pcre.org/pub/pcre/pcre-8.43.tar.gz
+tar xvfz pcre-8.43.tar.gz
+cd pcre-8.43
+./configure --prefix=/usr/local/pcre/8_43 --enable-jit
+make
+make install
+
+cd /root/nginx-install/development
+wget https://www.zlib.net/zlib-1.2.11.tar.gz
+tar zxvf zlib-1.2.11.tar.gz
+cd zlib-1.2.11
+./configure --prefix=$HOME/local/prior
+make
+make install
